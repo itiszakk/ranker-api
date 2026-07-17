dd20084a07e0a8224f84b6b6b5c20fd2d0475d4d
itiszakk <itiszakk@gmail.com>
2026-07-17 03:23:10 +0300

    Spring base

---
+++ src/test/java/org/itiszakk/ranker/RankerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.itiszakk.ranker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
